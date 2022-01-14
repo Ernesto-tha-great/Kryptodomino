d229acafb4341088edbaa0e6f1b4d5228bd73131
Ernest Nnamdi <ernest@Ernests-MacBook-Air.local>
2022-01-14 05:51:08 +0100

    navigation setup

---
+++ screens/EbookScreen.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import { View, Text } from 'react-native'
+
+const EbookScreen = () => {
+    return (
+        <View>
+            <Text>ebook</Text>
+        </View>
+    )
+}
+
+export default EbookScreen
--- types.tsx
+++ types.tsx
@@ -25,8 +25,11 @@ export type RootStackScreenProps<Screen extends keyof RootStackParamList> = Nati
 >;
 
 export type RootTabParamList = {
-  TabOne: undefined;
-  TabTwo: undefined;
+  Home: undefined;
+  Charts: undefined;
+  Favourite: undefined;
+  Ebooks: undefined;
+  Settings: undefined;
 };
 
 export type RootTabScreenProps<Screen extends keyof RootTabParamList> = CompositeScreenProps<
